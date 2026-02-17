1a70ae0917efb1efd49281f4c6705c181d7a17b1
Nurdin Hossain <nurdinhossain@Nurdins-MacBook-Air.local>
2026-02-17 14:20:10 -0500

    Add blank user home page

---
+++ src/app/userHome/page.js
@@ -0,0 +1,7 @@
+export default function UserHome() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-semibold">User Home</h1>
+    </div>
+  );
+}
